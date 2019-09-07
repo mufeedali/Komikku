mufeedali/Komikku

commit 2235fb4343158f9c587ebaf08fe9ad862ff69719

Updates must be stopped when exiting the application

mangascan/main_window.py
@@ -17,6 +17,7 @@ from threading import Timer
 from mangascan.add_dialog import AddDialog
 import mangascan.config_manager
 from mangascan.card import Card
+from mangascan.downloader import Downloader
 from mangascan.library import Library
 from mangascan.model import backup_db
 from mangascan.reader import Reader
@@ -38,6 +39,7 @@ class MainWindow(Gtk.ApplicationWindow):
         self.application = kwargs['application']
 
         self.logging_manager = self.application.get_logger()
+        self.downloader = Downloader(self)
         self.updater = Updater(self)
 
         self.builder = Gtk.Builder()
@@ -106,6 +108,7 @@ class MainWindow(Gtk.ApplicationWindow):
         pix = Pixbuf.new_from_resource_at_scale('/info/febvre/MangaScan/images/logo.png', 256, 256, True)
         self.builder.get_object('app_logo').set_from_pixbuf(pix)
 
+        # Init pages
         self.library = Library(self)
         self.card = Card(self)
         self.reader = Reader(self)
@@ -178,19 +181,28 @@ class MainWindow(Gtk.ApplicationWindow):
             self.save_window_size()
             backup_db()
 
-        if self.card.downloader.status == 'running':
+        if self.downloader.status == 'running' or self.updater.status == 'running':
             def confirm_callback():
-                self.card.downloader.stop()
+                self.downloader.stop()
+                self.updater.stop()
 
-                while self.card.downloader.status == 'running':
+                while self.downloader.status == 'running' or self.updater.status == 'running':
                     pass
 
                 before_quit()
                 self.application.quit()
 
+            message = [
+                _('Are you sure you want to quit?'),
+            ]
+            if self.downloader.status == 'running':
+                message.append(_('Some chapters are currently being downloaded.'))
+            if self.updater.status == 'running':
+                message.append(_('Some mangas are currently being updated.'))
+
             self.confirm(
                 _('Quit?'),
-                _('Are you sure you want to quit?\nSome chapters are currently being downloaded.'),
+                '\n'.join(message),
                 confirm_callback
             )
 
@@ -214,7 +226,8 @@ class MainWindow(Gtk.ApplicationWindow):
             else:
                 self.library.show(invalidate_sort=True)
         elif self.page == 'reader':
-            self.card.init()
+            self.card.update_chapter_row(self.reader.chapter)
+            self.card.show()
 
     def on_resize(self, window):
         size = self.get_size()

mangascan/card.py
@@ -5,14 +5,12 @@
 # Author: Valéry Febvre <vfebvre@easter-eggs.com>
 
 from gettext import gettext as _
-import threading
 
 from gi.repository import Gio
 from gi.repository import GLib
 from gi.repository import Gtk
 from gi.repository.GdkPixbuf import Pixbuf
 
-from mangascan.downloader import Downloader
 from mangascan.model import create_db_connection
 from mangascan.model import Download
 from mangascan.model import Manga
@@ -35,7 +33,7 @@ class Card():
 
         # Chapters listbox
         self.listbox = self.builder.get_object('chapters_listbox')
-        self.listbox.connect('row-activated', self.on_chapter_clicked)
+        self.listbox.connect('row-activated', self.on_chapter_row_clicked)
         self.gesture = Gtk.GestureLongPress.new(self.listbox)
         self.gesture.set_touch_only(False)
         self.gesture.connect('pressed', self.enter_selection_mode)
@@ -55,29 +53,6 @@ class Card():
                 return 0
 
         self.listbox.set_sort_func(sort)
-
-        # Downloader change callback
-        def downloader_change_cb(chapter):
-            if self.window.stack.props.visible_child_name != 'card':
-                return
-
-            if self.manga is None or self.manga.id != chapter.manga_id:
-                return
-
-            row = None
-            for child in self.listbox.get_children():
-                if child.chapter.id == chapter.id:
-                    row = child
-                    break
-
-            if row is None:
-                return
-
-            row.chapter = chapter
-            self.populate_chapter(row)
-
-        self.downloader = Downloader(downloader_change_cb)
-        self.downloader.start()
 
     @property
     def sort_order(self):
@@ -137,13 +112,12 @@ class Card():
             return
 
         # Add chapter in download queue
-        self.downloader.add(self.action_row.chapter.id)
-        self.downloader.add(self.action_row.chapter.id)
+        self.window.downloader.add(self.action_row.chapter.id)
 
         # Update chapter
-        self.populate_chapter(self.action_row)
+        self.populate_chapter_row(self.action_row)
 
-        self.downloader.start()
+        self.window.downloader.start()
 
     def download_selected_chapters(self, action, param):
         if not self.window.application.connected:
@@ -152,12 +126,12 @@ class Card():
 
         for row in self.listbox.get_selected_rows():
             # Add chapter in download queue
-            self.downloader.add(row.chapter.id)
+            self.window.downloader.add(row.chapter.id)
 
             # Update chapter
-            self.populate_chapter(row)
+            self.populate_chapter_row(row)
 
-        self.downloader.start()
+        self.window.downloader.start()
 
         self.leave_selection_mode()
 
@@ -186,6 +160,7 @@ class Card():
             self.manga = Manga.get(self.manga.id)
 
         self.populate()
+
         self.show(transition)
 
     def leave_selection_mode(self):
@@ -199,7 +174,7 @@ class Card():
 
         self.builder.get_object('menubutton').set_menu_model(self.builder.get_object('menu-card'))
 
-    def on_chapter_clicked(self, listbox, row):
+    def on_chapter_row_clicked(self, listbox, row):
         if self.selection_mode:
             if row._selected:
                 self.listbox.unselect_row(row)
@@ -255,7 +230,7 @@ class Card():
             row.chapter = chapter
             row._selected = False
 
-            self.populate_chapter(row)
+            self.populate_chapter_row(row)
 
             self.listbox.add(row)
 
@@ -290,7 +265,7 @@ class Card():
         self.builder.get_object('more_label').set_markup(
             '<i>{0}</i>'.format(_('Disk space used: {0}').format(folder_size(self.manga.path))))
 
-    def populate_chapter(self, row):
+    def populate_chapter_row(self, row):
         for child in row.get_children():
             child.destroy()
 
@@ -361,7 +336,7 @@ class Card():
 
         chapter.reset()
 
-        self.populate_chapter(self.action_row)
+        self.populate_chapter_row(self.action_row)
 
     def reset_selected_chapters(self, action, param):
         for row in self.listbox.get_selected_rows():
@@ -369,7 +344,7 @@ class Card():
 
             chapter.reset()
 
-            self.populate_chapter(row)
+            self.populate_chapter_row(row)
 
         self.leave_selection_mode()
 
@@ -379,9 +354,6 @@ class Card():
             self.listbox.invalidate_sort()
 
     def show(self, transition=True):
-        if self.window.stack.props.visible_child_name == 'card':
-            return
-
         self.title_label.set_text(self.manga.name)
 
         self.builder.get_object('left_button_image').set_from_icon_name('go-previous-symbolic', Gtk.IconSize.MENU)
@@ -421,7 +393,7 @@ class Card():
                 last_page_read_index=len(chapter.pages) - 1 if read else None,
             ))
 
-            self.populate_chapter(row)
+            self.populate_chapter_row(row)
 
         self.leave_selection_mode()
 
@@ -433,4 +405,19 @@ class Card():
             last_page_read_index=len(chapter.pages) - 1 if read else None,
         ))
 
-        self.populate_chapter(self.action_row)
+        self.populate_chapter_row(self.action_row)
+
+    def update_chapter_row(self, chapter):
+        """
+        Update a specific chapter row
+        - used when download status change (in Downloader)
+        - used when we come back from reader to update last page read
+        """
+        if self.window.page not in ('card', 'reader') or self.manga.id != chapter.manga_id:
+            return
+
+        for row in self.listbox.get_children():
+            if row.chapter.id == chapter.id:
+                row.chapter = chapter
+                self.populate_chapter_row(row)
+                break

mangascan/downloader.py
@@ -22,8 +22,9 @@ class Downloader():
     status = None
     stop_flag = False
 
-    def __init__(self, change_cb):
-        self.change_cb = change_cb
+    def __init__(self, window):
+        self.window = window
+        self.start()
 
     def add(self, chapter_id):
         Download.new(chapter_id)
@@ -88,17 +89,17 @@ class Downloader():
             )
             notification.show()
 
-            self.change_cb(chapter)
+            self.window.card.update_chapter_row(chapter)
 
             return False
 
         def error(chapter):
-            self.change_cb(chapter)
+            self.window.card.update_chapter_row(chapter)
 
             return False
 
         def start(chapter):
-            self.change_cb(chapter)
+            self.window.card.update_chapter_row(chapter)
 
             return False
 

commit 54fc4e8c5b3b668968846968654d5c016c381c98

[Card] Fixes a regression introduced in 2235fb43

mangascan/main_window.py
@@ -234,7 +234,11 @@ class MainWindow(Gtk.ApplicationWindow):
                 self.library.show(invalidate_sort=True)
         elif self.page == 'reader':
             self.set_unfullscreen()
-            self.card.update_chapter_row(self.reader.chapter)
+
+            # In card page, update all previously chapters consulted (last page read may have changed)
+            for chapter in self.reader.chapters_consulted:
+                self.card.update_chapter_row(chapter)
+
             self.card.show()
 
     def on_resize(self, window):

mangascan/reader.py
@@ -96,10 +96,6 @@ class Controls():
         self.scale.set_range(1, len(chapter.pages))
         self.nb_pages_label.set_text(str(len(chapter.pages)))
 
-    def init_fullscreen(self):
-        if mangascan.config_manager.get_fullscreen():
-            self.unfullscreen_button.emit('clicked')
-
     def on_fullscreen(self):
         if self.is_visible:
             self.top_box.show_all()
@@ -128,6 +124,7 @@ class Controls():
 class Reader():
     button_press_timeout_id = None
     chapter = None
+    chapters_consulted = []
     default_double_click_time = Gtk.Settings.get_default().get_property('gtk-double-click-time')
     pixbuf = None
     size = None
@@ -220,9 +217,13 @@ class Reader():
 
         if index is None:
             # We come from library
+            # Reset list of chapters consulted
+            self.chapters_consulted = []
             self.show()
 
         self.chapter = chapter
+        self.chapters_consulted.append(chapter)
+
         self.title_label.set_text(chapter.title)
 
         self.show_spinner()
@@ -475,7 +476,9 @@ class Reader():
         self.image.clear()
         self.pixbuf = None
         self.controls.hide()
-        self.controls.init_fullscreen()
+
+        if mangascan.config_manager.get_fullscreen():
+            self.window.set_fullscreen()
 
         self.window.show_page('reader')
 