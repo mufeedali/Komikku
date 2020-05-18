10b943827a9aef045fc5d98cb5be0d4a67b1a034
Valéry Febvre <vfebvre@easter-eggs.com>
2020-05-18 02:09:22 +0200

    [Servers] Izneo: Code cleanup
    + Fix author/copyright

--- komikku/servers/izneo.py
+++ komikku/servers/izneo.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2020 GrownNed
+# Copyright (C) 2020 tijder
 # SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
-# Author: GrownNed <grownned@gmail.com>
+# Author: tijder
 
 from bs4 import BeautifulSoup
-import re
-import requests
+import datetime
 import json
 
-from komikku.servers import convert_date_string
 from komikku.servers import get_buffer_mime_type
 from komikku.servers import Server
 
@@ -23,11 +21,11 @@ class Izneo(Server):
     base_url = 'https://yieha.izneo.com'
     base_reader_url = 'https://reader.izneo.com'
     login_url = base_url + '/nl/login'
-    manga_data_url = base_url + '/nl/api/serie/top/{}?abo=0'
-    manga_chapter_url = base_url + '/nl/api/library/detail/{0}?offset={1}&order=10&text='
-    manga_cover_url = base_url + '/nl/images/album/{}-275or365.jpg?v=undefined'
-    chapters_pages = base_reader_url + '/read/{}?startpage=1'
-    manga_collection_url = base_url + '/nl/api/library?offset={0}&order=3&text={1}'
+    api_manga_url = base_url + '/nl/api/serie/top/{0}?abo=0'
+    api_chapter_url = base_url + '/nl/api/library/detail/{0}?offset={1}&order=10&text='
+    cover_url = base_url + '/nl/images/album/{0}-275or365.jpg?v=undefined'
+    chapters_url = base_reader_url + '/read/{0}?startpage=1'
+    user_collection_url = base_url + '/nl/api/library?offset={0}&order=3&text={1}'
 
     def __init__(self, username=None, password=None):
         self.init(username, password)
@@ -37,42 +35,49 @@ class Izneo(Server):
         Returns manga data
         Initial data should contain at least manga's slug (provided by search)
         """
-        assert 'slug' in initial_data, 'Id is missing in the initial_data'
-        r = self.session_get(self.manga_data_url.format(initial_data['slug']))
+        assert 'slug' in initial_data, 'Manga slug is missing in initial_data'
+        r = self.session_get(self.api_manga_url.format(initial_data['slug']))
 
-        json_data = r.json()
+        try:
+            resp_data = r.json()
+        except Exception:
+            return None
+
         data = initial_data.copy()
         data.update(dict(
-            authors=[],
+            name=resp_data['name'],
+            authors=[author['nickname'] for author in resp_data['authors']],
             scanlators=[],
-            genres=[json_data['gender']],
+            genres=[resp_data['gender']],
             status='ongoing',
             chapters=[],
-            synopsis=json_data['synopsis'],
-            cover=self.manga_cover_url.format(json_data['ean_last_album']),
+            synopsis=resp_data['synopsis'],
+            cover=self.cover_url.format(resp_data['ean_last_album']),
             server_id=self.id,
-            url=self.base_url + json_data['url'],
+            url=resp_data['url'],
         ))
-
-        # Authors
-        for author in json_data['authors']:
-            data['authors'].append(author['nickname'])
 
         # Chapters
         offset = 0
-        while offset != None:
-            r = self.session_get(self.manga_chapter_url.format(initial_data['slug'], offset))
-            offset = offset + 1
-            json_data = r.json()
-            if 'albums' not in json_data:
+        while offset is not None:
+            r = self.session_get(self.api_chapter_url.format(initial_data['slug'], offset))
+            offset += 1
+            resp_data = r.json()
+            if 'albums' not in resp_data:
                 offset = None
-            else:
-                for chapter in json_data['albums'][str(offset)]:
-                    data['chapters'].append(dict(
-                        slug=chapter['ean'],
-                        title='{0} - {1}'.format(chapter['volume'], chapter['title']),
-                        date=None,
-                    ))
+                continue
+
+            for chapter in resp_data['albums'][str(offset)]:
+                title = chapter['title']
+                if chapter['volume']:
+                    title = '{0} - {1}'.format(chapter['volume'], title)
+
+                data['chapters'].append(dict(
+                    slug=chapter['ean'],
+                    title=title,
+                    date=datetime.datetime.fromtimestamp(chapter['version']).date(),
+                ))
+
         return data
 
     def get_manga_chapter_data(self, manga_slug, manga_name, chapter_slug, chapter_url):
@@ -81,10 +86,18 @@ class Izneo(Server):
 
         Currently, only pages are expected.
         """
-        r = self.session_get(self.chapters_pages.format(chapter_slug))
+        r = self.session_get(self.chapters_url.format(chapter_slug))
+        if r.status_code != 200:
+            return None
+
+        mime_type = get_buffer_mime_type(r.content)
+        if mime_type != 'text/html':
+            return None
+
         soup = BeautifulSoup(r.content, 'lxml')
-        j = soup.find('script').text.strip()
-        pages = json.loads(j.split('resourcesIndex               = ')[1].split(';')[0])
+
+        script_content = soup.find('script').string.strip()
+        pages = json.loads(script_content.split('resourcesIndex               = ')[1].split(';')[0])
 
         data = dict(
             pages=[],
@@ -93,8 +106,8 @@ class Izneo(Server):
         for page in pages:
             if page['id'] != 'endingpage':
                 data['pages'].append(dict(
-                    slug=page['id'],
-                    image=self.base_reader_url + page['url'],
+                    slug=None,
+                    image=page['url'],
                 ))
 
         return data
@@ -103,8 +116,8 @@ class Izneo(Server):
         """
         Returns chapter page scan (image) content
         """
-        r = self.session_get(page['image'])
-        if r is None or r.status_code != 200:
+        r = self.session_get(self.base_reader_url + page['image'])
+        if r.status_code != 200:
             return None
 
         mime_type = get_buffer_mime_type(r.content)
@@ -114,53 +127,32 @@ class Izneo(Server):
         return dict(
             buffer=r.content,
             mime_type=mime_type,
-            name=page['slug'],
+            name=page['image'].split('?')[0].split('/')[-1],
         )
 
-    @staticmethod
-    def get_manga_url(slug, url):
+    def get_manga_url(self, slug, url):
         """
         Returns manga absolute URL
         """
-        return url
+        return self.base_url + url
 
     def get_most_populars(self):
         """
-        Returns best noted manga list
+        Returns all series available in user's collection
         """
-        return self.__get_maga_collection('')
-
-    def search(self, term):
-        return self.__get_maga_collection(term)
-
-
-    def __get_maga_collection(self, term):
-        results = []
-        offset = 0
-        while offset != None:
-            r = self.session_get(self.manga_collection_url.format(offset, term))
-            offset = offset + 1
-            json_data = r.json()
-            if 'error' in json_data: # Not logged in
-                offset = None
-            elif 'series' not in json_data: # No more mangas to load
-                offset = None
-            else:
-                for comic in json_data['series']:
-                    results.append(dict(
-                        name=comic['name'],
-                        slug=comic['id'],
-                    ))
-        return results
+        return self.search_in_user_collection('')
 
     def login(self, username, password):
-        """
-        Log in and initializes API
-        """
         if not username or not password:
             return False
 
         r = self.session_get(self.login_url)
+        if r.status_code != 200:
+            return None
+
+        mime_type = get_buffer_mime_type(r.content)
+        if mime_type != 'text/html':
+            return None
 
         soup = BeautifulSoup(r.content, 'lxml')
 
@@ -170,18 +162,55 @@ class Izneo(Server):
                 'form[username]': username,
                 'form[password]': password,
                 'form[remember_me]': '1',
-                'form[_token]': soup.findAll('input', {u'type': u'hidden'})[0].get('value'),
+                'form[_token]': soup.findAll('input', {'type': 'hidden'})[0].get('value'),
             }
         )
 
-        r = self.session_get(self.manga_collection_url.format(0, ''))
-        j = r.json()
-        if 'error' in j:
+        r = self.session_get(self.user_collection_url.format(0, ''))
+        try:
+            if 'error' in r.json():
+                return False
+        except Exception:
             return False
 
         self.save_session()
 
         return True
+
+    def search(self, term):
+        return self.search_in_user_collection(term)
+
+    def search_in_user_collection(self, term):
+        results = []
+        offset = 0
+        while offset is not None:
+            r = self.session_get(self.user_collection_url.format(offset, term))
+            if r.status_code != 200:
+                return None
+
+            offset += 1
+            try:
+                resp_data = r.json()
+            except Exception:
+                return None
+
+            if 'error' in resp_data:
+                # Not logged in
+                return None
+
+            if 'series' not in resp_data:
+                # No more mangas to load
+                offset = None
+                continue
+
+            for serie in resp_data['series']:
+                results.append(dict(
+                    name=serie['name'],
+                    slug=serie['id'],
+                ))
+
+        return results
+
 
 class Yieha(Izneo):
     id = 'yieha:izneo'
