2845b9e0b7b87035d953d5f2e9c6e09d89616f4a
Valéry Febvre <vfebvre@easter-eggs.com>
2020-07-14 01:19:42 +0200

    [Servers] Reenable MangaSee
    Commit 8bd8dc62 was a mistake
    
    MangaSee is the main site, MangaLife is a clone instance used to test 
    new code

--- komikku/servers/mangasee.py
+++ komikku/servers/mangasee.py
@@ -5,81 +5,43 @@
 # Author: Valéry Febvre <vfebvre@easter-eggs.com>
 
 from bs4 import BeautifulSoup
-import re
+import json
 import requests
 
 from komikku.servers import convert_date_string
 from komikku.servers import get_buffer_mime_type
 from komikku.servers import Server
 from komikku.servers import USER_AGENT
-
-SERVER_NAME = 'MangaSee'
+from komikku.utils import log_error_traceback
 
 #
-# BEWARE: This server is disabled
-# Dead since 07/2020
-# It was moved (new url, new site) and is now a clone of MangaLife
+# MangaSee and MangaLife share exact same content
+# MangaSee is the main site, MangaLife is a clone instance used to test new code
 #
+
+headers = {
+    'User-Agent': USER_AGENT,
+    'Origin': 'https://mangasee123.com',
+}
 
 
 class Mangasee(Server):
     id = 'mangasee'
-    name = SERVER_NAME
+    name = 'MangaSee'
     lang = 'en'
-    status = 'disabled'
 
-    base_url = 'https://mangaseeonline.us'
-    search_url = base_url + '/search/request.php'
+    base_url = 'https://mangasee123.com'
+    search_url = base_url + '/search/'
     manga_url = base_url + '/manga/{0}'
     chapter_url = base_url + '/read-online/{0}-chapter-{1}-page-1.html'
-    page_url = base_url + '/read-online/{0}-chapter-{1}-page-{2}.html'
+    cover_url = 'https://cover.mangabeast01.com/cover/{0}.jpg'
+
+    mangas = None
 
     def __init__(self):
         if self.session is None:
             self.session = requests.Session()
-            self.session.headers.update({'user-agent': USER_AGENT})
-
-    @staticmethod
-    def decode_chapter_slug(chapter_slug):
-        if len(chapter_slug) != 6:
-            return chapter_slug
-
-        slug = int(chapter_slug[1:-1])
-        if chapter_slug[-1] != '0':
-            slug = f'{slug}.{chapter_slug[-1]}'
-        if chapter_slug[0] != '0':
-            slug = f'{slug}-index-{chapter_slug[0]}'
-
-        return slug
-
-    @staticmethod
-    def encode_chapter_slug(slug, title):
-        """Returns an encoded slug when chapter belongs to a season
-
-        We used a special format of 6 digits.
-        For example:
-        S3 - Chapter 100   => 301000
-        S2 - Chapter 101.5 => 201015
-
-        :param slug: Chapter slug
-        :param title: Chapter title
-        :return: Encoded slug if chapter belongs to a season else slug unchanged
-        """
-
-        match = re.search(r'S(\d) - ', title)
-        if not match:
-            return slug
-
-        season = match.group(1)
-
-        if '.' in slug:
-            slug = slug.replace('.', '')
-        else:
-            slug = slug + '0'
-
-        slug = '{0:05d}'.format(int(slug))
-
-        return season + slug
+            self.session.headers = headers
 
     def get_manga_data(self, initial_data):
         """
@@ -87,7 +49,7 @@ class Mangasee(Server):
 
         Initial data should contain at least manga's slug (provided by search)
         """
-        assert 'slug' in initial_data, 'Manga slug is missing in initial data'
+        assert 'slug' in initial_data, 'Slug is missing in initial data'
 
         r = self.session_get(self.manga_url.format(initial_data['slug']))
         if r.status_code != 200:
@@ -97,74 +59,97 @@ class Mangasee(Server):
         if mime_type != 'text/html':
             return None
 
-        soup = BeautifulSoup(r.text, 'html.parser')
-
         data = initial_data.copy()
         data.update(dict(
             authors=[],
-            scanlators=[],
+            scanlators=[],  # not available
             genres=[],
             status=None,
             synopsis=None,
             chapters=[],
             server_id=self.id,
-            cover=None,
+            cover=self.cover_url.format(data['slug']),
         ))
 
-        # Name & cover
-        data['name'] = soup.find('h1', class_='SeriesName').text.strip()
-        data['cover'] = soup.find('div', class_='leftImage').img.get('src')
+        soup = BeautifulSoup(r.content, 'lxml')
 
-        # Details & Synopsis
-        elements = soup.find('span', class_='details').find_all('div', class_='row')
-        for element in elements:
-            div_element = element.div
-            if div_element.b:
-                label = div_element.b.text.strip()
-            elif div_element.strong:
-                label = div_element.strong.text.strip()
+        data['name'] = soup.find('h1').text.strip()
+
+        for li_element in soup.find('ul', class_='list-group list-group-flush').find_all('li'):
+            if li_element.span is None:
+                continue
+
+            label = li_element.span.text.strip()
+            li_element.span.extract()
 
             if label.startswith('Author'):
-                links_elements = div_element.find_all('a')
-                for link_element in links_elements:
-                    data['authors'].append(link_element.text.strip())
+                data['authors'] = [artist.strip() for artist in li_element.text.split(',')]
 
             elif label.startswith('Genre'):
-                links_elements = div_element.find_all('a')
-                for link_element in links_elements:
-                    data['genres'].append(link_element.text.strip())
+                data['genres'] = [genre.strip() for genre in li_element.text.split(',')]
 
             elif label.startswith('Status'):
-                value = div_element.find_all('a')[0].text.replace('(Scan)', '').strip().lower()
-                if value in ('complete', 'hiatus', 'ongoing', ):
-                    data['status'] = value
-                elif value in ('cancelled', 'discontinued', ):
-                    data['status'] = 'suspended'
+                for status in li_element.text.split(','):
+                    if 'Scan' not in status:
+                        continue
+
+                    status = status.replace('(Scan)', '').strip().lower()
+                    if status in ('complete', 'hiatus', 'ongoing', ):
+                        data['status'] = status
+                    elif status in ('cancelled', 'discontinued', ):
+                        data['status'] = 'suspended'
+                    break
 
             elif label.startswith('Description'):
-                data['synopsis'] = div_element.div.text.strip()
+                data['synopsis'] = li_element.text.strip()
 
         # Chapters
-        elements = soup.find('div', class_='chapter-list').find_all('a', recursive=False)
-        for link_element in reversed(elements):
-            title = link_element.span.text.strip()
-            slug = link_element.get('chapter')
+        chapters = None
+        try:
+            script = soup.find_all('script')[-2].string
+            if script:
+                for line in script.split('\n'):
+                    line = line.strip()
+                    if not line.startswith('vm.Chapters'):
+                        continue
 
-            data['chapters'].append(dict(
-                slug=self.encode_chapter_slug(slug, title),
-                title=title,
-                date=convert_date_string(link_element.time.get('datestring').strip(), format='%Y%m%d'),
-            ))
+                    chapters = json.loads(line.split('=')[1].strip()[:-1])
+                    break
+        except Exception as e:
+            log_error_traceback(e)
+            return None
+
+        if chapters is not None:
+            for chapter in reversed(chapters):
+                slug = chapter['Chapter']
+
+                title = f'{chapter["Type"]} {int(chapter["Chapter"][1:-1])}'
+                if chapter['Chapter'][-1] != '0':
+                    title = f'{title}.{chapter["Chapter"][-1]}'
+                if chapter.get('ChapterName'):
+                    title = f'{title} - {chapter["ChapterName"]}'
+
+                data['chapters'].append(dict(
+                    slug=slug,
+                    title=title,
+                    date=convert_date_string(chapter['Date'], '%Y-%m-%d %H:%M:%S') if chapter.get('Date') else None,
+                ))
 
         return data
 
     def get_manga_chapter_data(self, manga_slug, manga_name, chapter_slug, chapter_url):
         """
-        Returns manga chapter data by scraping chapter HTML page content
+        Returns manga chapter data
 
         Currently, only pages are expected.
         """
-        r = self.session_get(self.chapter_url.format(manga_slug, self.decode_chapter_slug(chapter_slug)))
+        slug = int(chapter_slug[1:-1])
+        if chapter_slug[-1] != '0':
+            slug = f'{slug}.{chapter_slug[-1]}'
+        if chapter_slug[0] != '1':
+            slug = f'{slug}-index-{chapter_slug[0]}'
+
+        r = self.session_get(self.chapter_url.format(manga_slug, slug))
         if r.status_code != 200:
             return None
 
@@ -172,17 +157,43 @@ class Mangasee(Server):
         if mime_type != 'text/html':
             return None
 
-        soup = BeautifulSoup(r.text, 'html.parser')
+        soup = BeautifulSoup(r.content, 'lxml')
 
-        options_elements = soup.find('select', class_='PageSelect').find_all('option')
+        chapter = None
+        domain = None
+        try:
+            script = soup.find_all('script')[-2].string
+            if script:
+                for line in script.split('\n'):
+                    line = line.strip()
+                    if not line.startswith('vm.CurChapter') and not line.startswith('vm.CurPathName'):
+                        continue
+
+                    if line.startswith('vm.CurChapter'):
+                        chapter = json.loads(line.split('=')[1].strip()[:-1])
+                    elif line.startswith('vm.CurPathName'):
+                        domain = line.split('=')[1].strip()[1:-2]
+
+                    if chapter is not None and domain is not None:
+                        break
+        except Exception as e:
+            log_error_traceback(e)
+            return None
+
+        if chapter is None or domain is None:
+            return None
+
+        image_prefix = chapter_slug[1:-1]
+        if chapter['Directory']:
+            image_prefix = f'{chapter["Directory"]}/{image_prefix}'
 
         data = dict(
             pages=[],
         )
-        for option_element in options_elements:
+        for index in range(int(chapter['Page'])):
             data['pages'].append(dict(
-                slug=option_element.get('value'),
-                image=None,
+                slug=None,
+                image='https://{0}/manga/{1}/{2}-{3:03d}.png'.format(domain, manga_slug, image_prefix, index + 1),
             ))
 
         return data
@@ -191,14 +202,7 @@ class Mangasee(Server):
         """
         Returns chapter page scan (image) content
         """
-        r = self.session_get(self.page_url.format(manga_slug, self.decode_chapter_slug(chapter_slug), page['slug']))
-        if r.status_code != 200:
-            return None
-
-        soup = BeautifulSoup(r.text, 'html.parser')
-
-        url = soup.find('img', class_='CurImage').get('src')
-        r = self.session_get(url)
+        r = self.session_get(page['image'])
         if r.status_code != 200:
             return None
 
@@ -209,7 +213,7 @@ class Mangasee(Server):
         return dict(
             buffer=r.content,
             mime_type=mime_type,
-            name=url.split('/')[-1],
+            name=page['image'].split('/')[-1],
         )
 
     def get_manga_url(self, slug, url):
@@ -220,41 +224,74 @@ class Mangasee(Server):
 
     def get_most_populars(self):
         """
-        Returns most popular manga list
+        Returns most popular mangas list
         """
-        r = self.session_post(self.search_url, data=dict(page=1, sortBy='popularity', sortOrder='descending'))
-        if r.status_code != 200:
+        if self.mangas is None:
+            r = self.session_get(self.search_url)
+            if r.status_code != 200:
+                return None
+
+            mime_type = get_buffer_mime_type(r.content)
+            if mime_type != 'text/html':
+                return None
+
+            soup = BeautifulSoup(r.content, 'lxml')
+
+            try:
+                script = soup.find_all('script')[-2].string
+                if script:
+                    for line in script.split('\n'):
+                        line = line.strip()
+                        if not line.startswith('vm.Directory'):
+                            continue
+
+                        self.mangas = json.loads(line.split('vm.Directory = ')[1].strip()[:-1])
+                        break
+            except Exception as e:
+                log_error_traceback(e)
+                return None
+
+        if self.mangas is None:
             return None
 
-        mime_type = get_buffer_mime_type(r.content)
-        if mime_type != 'text/plain':
-            return None
-
-        soup = BeautifulSoup(r.text, 'html.parser')
+        self.mangas = sorted(self.mangas, key=lambda m: m['v'], reverse=True)
 
         results = []
-        for a_element in soup.find_all('a', class_='resultLink'):
+        for manga in self.mangas[:100]:
             results.append(dict(
-                name=a_element.text.strip(),
-                slug=a_element.get('href').split('/')[-1],
+                name=manga['s'],
+                slug=manga['i'],
             ))
 
         return results
 
     def search(self, term):
-        r = self.session_post(self.search_url, data=dict(keyword=term, page=1))
-        if r.status_code != 200:
-            return None
-
-        soup = BeautifulSoup(r.content, 'html.parser')
-
         results = []
-        for element in soup.find_all('div', class_='requested'):
-            link_element = element.find('a', class_='resultLink')
+        term = term.lower()
+
+        if self.mangas is None:
+            if self.get_most_populars() is None:
+                return None
+
+        for manga in self.mangas:
+            if term not in manga['s'].lower():
+                continue
 
             results.append(dict(
-                slug=link_element.get('href').split('/')[-1],
-                name=link_element.text.strip(),
+                name=manga['s'],
+                slug=manga['i'],
             ))
 
         return results
+
+
+class Mangalife(Mangasee):
+    id = 'mangalife:mangasee'
+    name = 'MangaLife'
+    lang = 'en'
+    status = 'disabled'
+
+    base_url = 'https://manga4life.com'
+    search_url = base_url + '/search/'
+    manga_url = base_url + '/manga/{0}'
+    chapter_url = base_url + '/read-online/{0}-chapter-{1}-page-1.html'
