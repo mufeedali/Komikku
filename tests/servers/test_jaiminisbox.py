0fb5aa9b4df9c97a6e8e53ad5220e2b7db28517c
Valéry Febvre <vfebvre@easter-eggs.com>
2020-11-20 01:08:33 +0100

    [Tests] Fix Jaimini's box

--- tests/servers/test_jaiminisbox.py
+++ tests/servers/test_jaiminisbox.py
@@ -8,18 +8,32 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 @pytest.fixture
-def jaiminisbox_server():
-    from komikku.servers.jaiminisbox import Jaiminisbox
+def kireicake_server():
+    from komikku.servers.jaiminisbox import Kireicake
 
-    return Jaiminisbox()
+    return Kireicake()
+
+
+@pytest.fixture
+def lupiteam_server():
+    from komikku.servers.jaiminisbox import Lupiteam
+
+    return Lupiteam()
+
+
+@pytest.fixture
+def tuttoanimemanga_server():
+    from komikku.servers.jaiminisbox import Tuttoanimemanga
+
+    return Tuttoanimemanga()
 
 
 @test_steps('get_most_popular', 'search', 'get_manga_data', 'get_chapter_data', 'get_page_image')
-def test_jaiminisbox(jaiminisbox_server):
+def test_kireicake(kireicake_server):
     # Get most popular
     print('Get most popular')
     try:
-        response = jaiminisbox_server.get_most_populars()
+        response = kireicake_server.get_most_populars()
     except Exception as e:
         response = None
         log_error_traceback(e)
@@ -30,7 +44,7 @@ def test_jaiminisbox(jaiminisbox_server):
     # Search
     print('Search')
     try:
-        response = jaiminisbox_server.search('oshi no ko')
+        response = kireicake_server.search('Dai Dark')
         slug = response[0]['slug']
     except Exception as e:
         slug = None
@@ -42,7 +56,7 @@ def test_jaiminisbox(jaiminisbox_server):
     # Get manga data
     print('Get manga data')
     try:
-        response = jaiminisbox_server.get_manga_data(dict(slug=slug))
+        response = kireicake_server.get_manga_data(dict(slug=slug))
         chapter_slug = response['chapters'][0]['slug']
     except Exception as e:
         chapter_slug = None
@@ -54,7 +68,7 @@ def test_jaiminisbox(jaiminisbox_server):
     # Get chapter data
     print("Get chapter data")
     try:
-        response = jaiminisbox_server.get_manga_chapter_data(slug, None, chapter_slug, None)
+        response = kireicake_server.get_manga_chapter_data(slug, None, chapter_slug, None)
         page = response['pages'][0]
     except Exception as e:
         page = None
@@ -66,7 +80,129 @@ def test_jaiminisbox(jaiminisbox_server):
     # Get page image
     print('Get page image')
     try:
-        response = jaiminisbox_server.get_manga_chapter_page_image(None, None, None, page)
+        response = kireicake_server.get_manga_chapter_page_image(None, None, None, page)
+    except Exception as e:
+        response = None
+        log_error_traceback(e)
+
+    assert response is not None
+    yield
+
+
+@test_steps('get_most_popular', 'search', 'get_manga_data', 'get_chapter_data', 'get_page_image')
+def test_lupiteam(lupiteam_server):
+    # Get most popular
+    print('Get most popular')
+    try:
+        response = lupiteam_server.get_most_populars()
+    except Exception as e:
+        response = None
+        log_error_traceback(e)
+
+    assert response is not None
+    yield
+
+    # Search
+    print('Search')
+    try:
+        response = lupiteam_server.search('one piece')
+        slug = response[0]['slug']
+    except Exception as e:
+        slug = None
+        log_error_traceback(e)
+
+    assert slug is not None
+    yield
+
+    # Get manga data
+    print('Get manga data')
+    try:
+        response = lupiteam_server.get_manga_data(dict(slug=slug))
+        chapter_slug = response['chapters'][1]['slug']
+    except Exception as e:
+        chapter_slug = None
+        log_error_traceback(e)
+
+    assert chapter_slug is not None
+    yield
+
+    # Get chapter data
+    print("Get chapter data")
+    try:
+        response = lupiteam_server.get_manga_chapter_data(slug, None, chapter_slug, None)
+        page = response['pages'][0]
+    except Exception as e:
+        page = None
+        log_error_traceback(e)
+
+    assert page is not None
+    yield
+
+    # Get page image
+    print('Get page image')
+    try:
+        response = lupiteam_server.get_manga_chapter_page_image(None, None, None, page)
+    except Exception as e:
+        response = None
+        log_error_traceback(e)
+
+    assert response is not None
+    yield
+
+
+@test_steps('get_most_popular', 'search', 'get_manga_data', 'get_chapter_data', 'get_page_image')
+def test_tuttoanimemanga(tuttoanimemanga_server):
+    # Get most popular
+    print('Get most popular')
+    try:
+        response = tuttoanimemanga_server.get_most_populars()
+    except Exception as e:
+        response = None
+        log_error_traceback(e)
+
+    assert response is not None
+    yield
+
+    # Search
+    print('Search')
+    try:
+        response = tuttoanimemanga_server.search('One piece')
+        slug = response[0]['slug']
+    except Exception as e:
+        slug = None
+        log_error_traceback(e)
+
+    assert slug is not None
+    yield
+
+    # Get manga data
+    print('Get manga data')
+    try:
+        response = tuttoanimemanga_server.get_manga_data(dict(slug=slug))
+        chapter_slug = response['chapters'][0]['slug']
+    except Exception as e:
+        chapter_slug = None
+        log_error_traceback(e)
+
+    assert chapter_slug is not None
+    yield
+
+    # Get chapter data
+    print("Get chapter data")
+    try:
+        response = tuttoanimemanga_server.get_manga_chapter_data(slug, None, chapter_slug, None)
+        page = response['pages'][0]
+    except Exception as e:
+        page = None
+        log_error_traceback(e)
+
+    assert page is not None
+    yield
+
+    # Get page image
+    print('Get page image')
+    try:
+        response = tuttoanimemanga_server.get_manga_chapter_page_image(None, None, None, page)
     except Exception as e:
         response = None
         log_error_traceback(e)
