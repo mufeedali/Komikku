1733d6c231b0f702f95872c2ab2ac2b2495a34d7
Valéry Febvre <vfebvre@easter-eggs.com>
2020-11-24 01:00:38 +0100

    Update screenshots

--- take_screenshots.sh
+++ take_screenshots.sh
@@ -7,12 +7,15 @@ ninja -C _build install
 GTK_THEME=Adwaita:dark ninja -C _build run &
 
 sleep 5
-wmctrl -F -r Komikku -e 0,0,0,412,834
+wmctrl -F -r Komikku -e 0,0,0,412,838
+wmctrl -F -r Komikku -N "Komikku main"
 
 sleep 5
 gnome-screenshot -w -f screenshots/main-window.png
 
-xdotool key --delay 100 Shift+Tab Shift+Tab Shift+Tab Return
+xdotool key --delay 100 Return
+sleep 1
+wmctrl -F -r Komikku -e 0,0,0,412,838
 sleep 1
 gnome-screenshot -w -f screenshots/add-servers.png
 sleep 3
@@ -20,6 +23,8 @@ sleep 3
 xdotool key --delay 100 Shift+Tab Return
 xdotool key --delay 100 Tab Tab Return
 xdotool key --delay 100 Tab Tab Return
+sleep 1
+wmctrl -F -r Preferences -e 0,0,0,412,838
 sleep 1
 gnome-screenshot -w -f screenshots/preferences.png
 sleep 3
@@ -34,8 +39,8 @@ sleep 0.5
 gnome-screenshot -w -f screenshots/card-chapters.png
 sleep 3
 
-xdotool key --delay 100 Tab Tab Tab Return
-xdotool mousemove -sync 200 500 click 1
+xdotool key --delay 100 Tab Return
+# xdotool mousemove -sync 200 500 click 1
 sleep 2
 gnome-screenshot -w -f screenshots/reader.png
 sleep 3
