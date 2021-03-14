mufeedali/Komikku

commit 0517e9e7215acccbd853acb73badcd4e85e1e5b8

application: Hide categories button when empty
It's useless anyway

komikku/library.py
@@ -455,11 +455,13 @@ class Library:
 
             # Display first start message
             self.window.box.add(self.window.first_start_grid)
+            self.flap_reveal_button.hide()
 
             return
 
         if self.window.first_start_grid.is_ancestor(self.window):
             self.window.box.remove(self.window.first_start_grid)
+            self.flap_reveal_button.show()
 
         if not self.window.overlay.is_ancestor(self.window):
             self.window.box.add(self.window.overlay)
@@ -495,8 +497,6 @@ class Library:
 
     def show(self, invalidate_sort=False):
         self.window.left_button_image.set_from_icon_name('list-add-symbolic', Gtk.IconSize.MENU)
-
-        self.flap_reveal_button.show()
 
         self.search_button.show()
         self.window.card.resume_read_button.hide()

commit c8426f545361aa1713a93aafe0b0a00b3e186711

Fix a regression introduced in 0517e9e7
Going from Library to Card and then back would keep the categories
button hidden. Added an if condition to fix the issue caused by keeping
the original code.

komikku/library.py
@@ -498,6 +498,9 @@ class Library:
     def show(self, invalidate_sort=False):
         self.window.left_button_image.set_from_icon_name('list-add-symbolic', Gtk.IconSize.MENU)
 
+        if self.window.overlay.is_ancestor(self.window):
+            self.flap_reveal_button.show()
+
         self.search_button.show()
         self.window.card.resume_read_button.hide()
         self.window.reader.fullscreen_button.hide()