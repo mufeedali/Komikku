1d7f22a365ad8eead9b306fa2c0338e6b7482be4
Valéry Febvre <vfebvre@easter-eggs.com>
2021-03-14 02:21:31 +0100

    Update Copyright date

--- komikku/servers/manganelo/__init__.py
+++ komikku/servers/manganelo/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2019-2020 Valéry Febvre
+# Copyright (C) 2019-2021 Valéry Febvre
 # SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
 # Author: Valéry Febvre <vfebvre@easter-eggs.com>
 
