5ba2d159efe2a1ebf54c8bcaaf03fe5814cd6b36
Liliana Prikler <liliana.prikler@gmail.com>
2021-09-11 15:51:48 +0200

    [Servers] Add Bilibili.

---
+++ tests/servers/test_bilibili.py
@@ -0,0 +1,76 @@
+import logging
+import pytest
+from pytest_steps import test_steps, optional_step
+
+from komikku.utils import log_error_traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@pytest.fixture
+def bilibili_server():
+    from komikku.servers.bilibili import Bilibili
+
+    return Bilibili()
+
+
+@test_steps('get_most_popular', 'search', 'get_manga_data', 'get_chapter_data', 'get_page_image')
+def test_bilibili(bilibili_server):
+    # Get most popular
+    print('Get most popular')
+    with optional_step('get_most_popular') as step:
+        try:
+            response = bilibili_server.get_most_populars()
+        except Exception as e:
+            response = None
+            log_error_traceback(e)
+
+        assert response is not None
+    yield step
+
+    # Search
+    print('Search')
+    try:
+        response = bilibili_server.search('working overtime to destroy the world') # or maybe don't, because bilibili only matches "the"
+        slug = response[0]['slug']
+    except Exception as e:
+        slug = None
+        log_error_traceback(e)
+
+    assert slug is not None
+    yield
+
+    # Get manga data
+    print('Get manga data')
+    try:
+        response = bilibili_server.get_manga_data(dict(slug=slug))
+        chapter_slug = response['chapters'][0]['slug']
+    except Exception as e:
+        chapter_slug = None
+        log_error_traceback(e)
+
+    assert chapter_slug is not None
+    yield
+
+    # Get chapter data
+    print("Get chapter data")
+    try:
+        response = bilibili_server.get_manga_chapter_data(None, None, chapter_slug, None)
+        page = response['pages'][0]
+    except Exception as e:
+        page = None
+        log_error_traceback(e)
+
+    assert page is not None
+    yield
+
+    # Get page image
+    print('Get page image')
+    try:
+        response = bilibili_server.get_manga_chapter_page_image(None, None, chapter_slug, page)
+    except Exception as e:
+        response = None
+        log_error_traceback(e)
+
+    assert response is not None
+    yield
